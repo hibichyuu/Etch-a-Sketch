1ddc7793dc54f7435c1b998f6c27577ab5b53b88
hibichyuu <hibichyuu@gmail.com>
2024-02-10 10:09:08 -0600

    Add functionality options
        Makes an option ui so the user has more
    freedom in terms of color, grid size, and erasing.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,58 @@
 const pixelDiv = document.querySelector("#pixelContainer");
 
-createPixels();
+const restartBtn = document.querySelector("#restartButton");
+restartBtn.addEventListener("click", ()=>{restartGrid();});
 
+const colorSelect = document.querySelector("#colorSelect");
+colorSelect.addEventListener("change",()=>{changeColor(colorSelect.value)});
+
+const eraserBtn = document.querySelector("#eraser");
+eraserBtn.addEventListener('click', ()=>{pixelColor = "white";});
+
+const penBtn = document.querySelector("#pen");
+penBtn.addEventListener('click', ()=>{changeColor(colorSelect.value);});
+
+const gridSelect = document.querySelector("#gridSelect");
+gridSelect.addEventListener("change", ()=>{changeGridLength(parseInt(gridSelect.value));});
+
+let gridLength = 16;
+let pixelColor = "black";
+
+createPixels();
 
 function createPixels(){
 
-    for(let i = 1; i<=16; i++){
+    for(let i = 1; i<=gridLength; i++){
         let row = document.createElement("div");
         row.classList.add("row");
-        for(let j = 1; j<=16; j++){
+        for(let j = 1; j<=gridLength; j++){
             let pixel = document.createElement("div");
             pixel.classList.add("pixel");
             row.appendChild(pixel);
+            addPixelListeners(pixel);
         }
         pixelDiv.appendChild(row);
     }
 
+}
+
+function addPixelListeners(pixel){
+    pixel.addEventListener("mouseover", ()=>{
+        pixel.style.backgroundColor = pixelColor;
+    });
+}
+
+function restartGrid(){
+    pixelDiv.textContent = '';
+    createPixels();
+}
+
+function changeColor(color){
+    
+        pixelColor = color;
+
+}
+
+function changeGridLength(length){
+    gridLength = length;
 }
